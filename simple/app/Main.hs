36a5bc4e90c9926e17f22fda8b298a4accbb4819
Aaron McDaid <aaronmcdaid@gmail.com>
2017-11-01 17:47:39 +0000

    Really ugly example, but it works. Slaves print a message directly themselves, instead of sending it back

--- simple/app/Main.hs
+++ simple/app/Main.hs
@@ -1,12 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import System.Environment (getArgs)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node (initRemoteTable)
 import Control.Distributed.Process.Backend.SimpleLocalnet
 
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
+sampleTask :: (Int, String) -> Process ()
+sampleTask (t, s) = liftIO (print "hi" >> threadDelay (t * 1000000)) >>
+                        say s
+
+remotable ['sampleTask]
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
+
 master :: Backend -> [NodeId] -> Process ()
 master backend slaves = do
    -- Do something interesting with the slaves
    liftIO . putStrLn $ "Slaves: " ++ show slaves
+   pid <- spawn (slaves !! 0) $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
+   pid <- spawn (slaves !! 1) $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
+   pid <- spawn (slaves !! 2) $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
    -- Terminate the slaves when the master terminates (this is optional)
    terminateAllSlaves backend
 
@@ -16,10 +36,10 @@ main = do
 
    case args of
      ["master", host, port] -> do
-       backend <- initializeBackend host port initRemoteTable
+       backend <- initializeBackend host port myRemoteTable
        startMaster backend (master backend)
      ["slave", host, port] -> do
-       backend <- initializeBackend host port initRemoteTable
+       backend <- initializeBackend host port myRemoteTable
        startSlave backend
 {-
 import Control.Concurrent (threadDelay)
