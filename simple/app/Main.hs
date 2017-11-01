3ed0372ebed136ee78911532c92e66b6f2175308
Aaron McDaid <aaronmcdaid@gmail.com>
2017-11-01 15:39:33 +0000

    accept the command line args. Just print them out for now

--- simple/app/Main.hs
+++ simple/app/Main.hs
@@ -4,6 +4,45 @@ import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 
+-- next two for options processing, https://stackoverflow.com/a/39049486/146041
+import Options.Applicative
+import Data.Monoid ((<>))
+
+{-
+ - Program should accept the two command line arguments: --send-for k, which
+ - denotes how many seconds does the system send messages, and --wait-for l,
+ - which denotes the length of the grace period in seconds. s, k, l ∈ N. We also
+ - suggest providing --with-seed s argument, which defines seed for RNGs.
+ -}
+data Sample = Sample
+    { k :: Int -- send-for
+    , l :: Int -- wait-for
+    , s :: Int -- with-seed
+    } deriving Show
+
+sample :: Parser Sample
+sample = Sample
+     <$> option auto
+          ( long "send-for"
+         <> short 'k'
+         <> help "how many seconds does the system send messages"
+         <> showDefault
+         <> value (-1)
+         <> metavar "INT" )
+     <*> option auto
+          ( long "wait-for"
+         <> short 'l'
+         <> help "the length of the grace period in seconds"
+         <> showDefault
+         <> value (-1)
+         <> metavar "INT" )
+     <*> option auto
+          ( long "with-seed"
+         <> short 's'
+         <> help "which defines seed for RNGs"
+         <> showDefault
+         <> value (-1)
+         <> metavar "INT" )
 replyBack :: (ProcessId, String) -> Process ()
 replyBack (sender, msg) = send sender ("<" ++ msg ++ ">")
 
@@ -12,6 +51,11 @@ logMessage msg = say $ "handling " ++ msg
 
 main :: IO ()
 main = do
+  let opts = info (helper <*> sample)
+          ( fullDesc
+         <> progDesc "Replicate a string"
+         <> header "repstring - an example of the optparse-applicative package" )
+  execParser opts >>= print
   Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
   node <- newLocalNode t initRemoteTable
   runProcess node $ do
