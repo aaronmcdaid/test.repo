2fab80c02d0dc96411262a3533257ece9f01f66d
Aaron McDaid <aaronmcdaid@gmail.com>
2017-11-01 15:05:03 +0000

    first example from that tutorial

--- simple/app/Main.hs
+++ simple/app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Distributed.Process
+import Control.Distributed.Process.Node
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+    node <- newLocalNode t initRemoteTable
+    print "hi"
+    _ <- runProcess node $ do
+        -- get our own process id
+        self <- getSelfPid
+        send self "hello"
+        hello <- expect :: Process String
+        liftIO $ putStrLn hello
+    return ()
