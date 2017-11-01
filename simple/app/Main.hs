e94760ddf4cc6f79e5bd21046377550d9c644c23
Aaron McDaid <aaronmcdaid@gmail.com>
2017-11-01 15:17:56 +0000

    the next example. I used an extra 'forever' to get multiple replies

--- simple/app/Main.hs
+++ simple/app/Main.hs
@@ -1,20 +1,44 @@
-module Main where
-
-import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
 
-import Lib
+replyBack :: (ProcessId, String) -> Process ()
+replyBack (sender, msg) = send sender ("<" ++ msg ++ ">")
+
+logMessage :: String -> Process ()
+logMessage msg = say $ "handling " ++ msg
 
 main :: IO ()
 main = do
-    Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
-    node <- newLocalNode t initRemoteTable
-    print "hi"
-    _ <- runProcess node $ do
-        -- get our own process id
-        self <- getSelfPid
-        send self (3.1415 ::        Double)
-        hello <- expect :: Process  Double
-        liftIO $ putStrLn (show [hello])
-    return ()
+  Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+  node <- newLocalNode t initRemoteTable
+  runProcess node $ do
+    -- Spawn another worker on the local node
+    echoPid <- spawnLocal $ forever $ do
+      -- Test our matches in order against each message in the queue
+      receiveWait [match logMessage, match replyBack]
+
+    -- The `say` function sends a message to a process registered as "logger".
+    -- By default, this process simply loops through its mailbox and sends
+    -- any received log message strings it finds to stderr.
+
+    say "send some messages!"
+    send echoPid "hello"
+    send echoPid "Hello"
+    self <- getSelfPid
+    send echoPid (self, "hello")
+    send echoPid (self, "Hello")
+    send echoPid (self, "Hello")
+
+    -- `expectTimeout` waits for a message or times out after "delay"
+    forever $ do
+        m <- expectTimeout 1000000 -- one second == 1million ms
+        case m of
+          -- Die immediately - throws a ProcessExitException with the given reason.
+          Nothing  -> die "nothing came back!"
+          Just s -> say $ "got " ++ s ++ " back!"
+
+    -- Without the following delay, the process sometimes exits before the messages are exchanged.
+    liftIO $ threadDelay 2000000
